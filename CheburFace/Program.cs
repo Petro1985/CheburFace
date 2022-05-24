6cbdb6ac677941cf50db0c1b223babba9ca6f08f
Petr <Petro1985p@gmail.com>
2022-05-24 17:59:28 +0300

    Refactoring of message view has been made

--- CheburFace/Program.cs
+++ CheburFace/Program.cs
@@ -12,7 +12,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
+
+var q = Func();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -24,3 +27,12 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+
+
+
+int Func()
+{
+    var a = 0;
+    return a++;
+}
